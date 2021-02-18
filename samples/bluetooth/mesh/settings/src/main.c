5edd3577131adbf16df88342e918b08ba3d0be66
Erik Robstad <Erik.Robstad@nordicsemi.no>
2021-02-18 12:05:49 +0100

    Modified sample folder

--- samples/bluetooth/mesh/settings/src/main.c
+++ samples/bluetooth/mesh/settings/src/main.c
@@ -1,11 +1,120 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
 
 #include <bluetooth/bluetooth.h>
+#include <bluetooth/mesh.h>
 #include <bluetooth/mesh/models.h>
 #include <bluetooth/mesh/dk_prov.h>
 #include <dk_buttons_and_leds.h>
-//#include "model_handler.h"
 
-// Mesh Initialization
+// ONOFF Server
+
+static void led_set(struct bt_mesh_onoff_srv *srv, struct bt_mesh_msg_ctx *ctx,
+		    const struct bt_mesh_onoff_set *set,
+		    struct bt_mesh_onoff_status *rsp)
+{
+	printk("LED set\n");
+}
+
+static void led_get(struct bt_mesh_onoff_srv *srv, struct bt_mesh_msg_ctx *ctx,
+		    struct bt_mesh_onoff_status *rsp)
+{
+	printk("LED get\n");
+}
+
+static const struct bt_mesh_onoff_srv_handlers onoff_handlers = {
+	.set = led_set,
+	.get = led_get,
+};
+
+static struct bt_mesh_onoff_srv onoff_srv = BT_MESH_ONOFF_SRV_INIT(&onoff_handlers);
+
+// *********************** SNURRE I GANG BT MESH ************************
+
+/** Configuration server definition */
+static struct bt_mesh_cfg_srv cfg_srv = {
+	.relay = IS_ENABLED(CONFIG_BT_MESH_RELAY),
+	.beacon = BT_MESH_BEACON_ENABLED,
+	.frnd = IS_ENABLED(CONFIG_BT_MESH_FRIEND),
+	.gatt_proxy = IS_ENABLED(CONFIG_BT_MESH_GATT_PROXY),
+	.default_ttl = 7,
+
+	/* 3 transmissions with 20ms interval */
+	.net_transmit = BT_MESH_TRANSMIT(2, 20),
+	.relay_retransmit = BT_MESH_TRANSMIT(2, 20),
+};
+
+/* Set up a repeating delayed work to blink the DK's LEDs when attention is
+ * requested.
+ */
+static struct k_delayed_work attention_blink_work;
+
+static void attention_blink(struct k_work *work)
+{
+	static int idx;
+	const uint8_t pattern[] = {
+		BIT(0) | BIT(1),
+		BIT(1) | BIT(2),
+		BIT(2) | BIT(3),
+		BIT(3) | BIT(0),
+	};
+
+	dk_set_leds(pattern[idx++ % ARRAY_SIZE(pattern)]);
+	k_delayed_work_submit(&attention_blink_work, K_MSEC(30));
+}
+
+static void attention_on(struct bt_mesh_model *mod)
+{
+	k_delayed_work_submit(&attention_blink_work, K_NO_WAIT);
+}
+
+static void attention_off(struct bt_mesh_model *mod)
+{
+	k_delayed_work_cancel(&attention_blink_work);
+	dk_set_leds(DK_NO_LEDS_MSK);
+}
+
+static const struct bt_mesh_health_srv_cb health_srv_cb = {
+	.attn_on = attention_on,
+	.attn_off = attention_off,
+};
+
+static struct bt_mesh_health_srv health_srv = {
+	.cb = &health_srv_cb,
+};
+
+BT_MESH_HEALTH_PUB_DEFINE(health_pub, 0);
+
+// ENDRES TIL KORREKT MODELL
+static struct bt_mesh_elem elements[] = {
+	BT_MESH_ELEM(1,
+		     BT_MESH_MODEL_LIST(BT_MESH_MODEL_CFG_SRV(&cfg_srv),
+					BT_MESH_MODEL_HEALTH_SRV(&health_srv,
+								 &health_pub),
+					BT_MESH_MODEL_ONOFF_SRV(&onoff_srv)), // BARE ENDRE DENNE LINJEN
+		     BT_MESH_MODEL_NONE),
+};
+
+static const struct bt_mesh_comp comp = {
+	.cid = CONFIG_BT_COMPANY_ID,
+	.elem = elements,
+	.elem_count = ARRAY_SIZE(elements),
+};
+
+const struct bt_mesh_comp *model_handler_init(void)
+{
+	k_delayed_work_init(&attention_blink_work, attention_blink);
+
+	// INITIALIZATION IF NEEDED
+
+	return &comp;
+}
+
+// *********************************************************************
+
 static void bt_ready(int err)
 {
 	if (err) {
@@ -40,21 +149,8 @@ void main(void)
 
 	printk("Initializing...\n");
 
-	/* Initialize the Bluetooth Subsystem */
 	err = bt_enable(bt_ready);
-        
-       // Add tx gain settings
-        static const uint8_t txp[] = {8, 4, 0, -4};
-        uint8_t tx = txp[3];
-	
-        set_tx_power(BT_HCI_VS_LL_HANDLE_TYPE_CONN,
-				     default_conn_handle,
-				     BT_HCI_VS_LL_TX_POWER_LEVEL_NO_PREF);
-					
-		get_tx_power(BT_HCI_VS_LL_HANDLE_TYPE_CONN,
-				     default_conn_handle, &txp);
-	
-        if (err) {
+	if (err) {
 		printk("Bluetooth init failed (err %d)\n", err);
 	}
 }

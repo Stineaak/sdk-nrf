8355d9e3b594c364dc31a53be98c4185f15b816f
Stine Åkredalen <stineakredalen@Stines-MacBook-Pro.local>
2021-02-22 22:21:28 +0100

    updated cli/srv files

---
+++ samples/bluetooth/mesh/settings/include/settings_cli.h
@@ -0,0 +1,32 @@
+
+#include <bluetooth/mesh/model_types.h>
+#include "settings_mod.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Node composition data - The node composition data is passed to bt_mesh_init()
+
+extern const struct bt_mesh_model_op _bt_mesh_settings_cli_op[];
+extern const struct bt_mesh_model_cb _bt_mesh_settings_cli_cb;
+static struct bt_mesh_model_pub pub_ctx;
+
+// DO: Define bt_mesh_settings_cli ...
+
+#define BT_MESH_SETTINGS_CLI_INIT(_status_handler)                                \
+	{                                                                      \
+		.status_handler = _status_handler,                             \
+		.pub = {.msg = NET_BUF_SIMPLE(BT_MESH_MODEL_BUF_LEN(           \
+				BT_MESH_DEVICE_SETTINGS_SET_OP,                          \
+				BT_MESH_DEVICE_SETTINGS_MSG_MAXLEN_SET)) }               \
+	}
+
+#define BT_MESH_MODEL_SETTINGS_CLI(_cli)	
+    BT_MESH_MODEL_VND_CB(
+        BT_MESH_NORDIC_SEMI_COMPANY_ID, BT_MESH_MODEL_ID_SETTINGS_CLI,
+        _bt_mesh_settings_cli_op,
+        &pub_ctx, // for publishing SET messages // &(_cli)->pub (?)
+        BT_MESH_MODEL_USER_DATA(struct bt_mesh_settings_cli, _cli),
+        &_bt_mesh_settings_cli_cb) // enable callbacks
+
---
+++ samples/bluetooth/mesh/settings/include/settings_mod.h
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include "model_utils.h"
+#include <bluetooth/mesh/models.h>
+
+// Defining model identifiers
+
+#define BT_MESH_NORDIC_SEMI_COMPANY_ID 0x0059
+#define BT_MESH_MODEL_ID_SETTINGS_CLI 0x0005
+#define BT_MESH_MODEL_ID_SETTINGS_SRV 0x0006 
+
+// Defining OPcodes for messages
+
+#define BT_MESH_DEVICE_SETTINGS_GET_OP  
+    BT_MESH_MODEL_OP_3(0x01, BT_MESH_NORDIC_SEMI_COMPANY_ID)
+#define BT_MESH_DEVICE_SETTINGS_SET_OP    
+    BT_MESH_MODEL_OP_3(0x02, BT_MESH_NORDIC_SEMI_COMPANY_ID)
+#define BT_MESH_DEVICE_SETTINGS_STATUS_OP    
+    BT_MESH_MODEL_OP_3(0x03, BT_MESH_NORDIC_SEMI_COMPANY_ID)
+
+#define BT_MESH_DEVICE_SETTINGS_MSG_MINLEN_GET 0 
+#define BT_MESH_DEVICE_SETTINGS_MSG_MINLEN_SET 2
+#define BT_MESH_DEVICE_SETTINGS_MSG_MAXLEN_SET 5
+#define BT_MESH_DEVICE_SETTINGS_MSG_MINLEN_STATUS 2
+
+/** Mandatory parameters for the Settings Set message. */
+struct bt_mesh_settings_set {
+	/** State to set. */
+	int8_t txp_value;
+	/** Transition parameters. */
+	const struct bt_mesh_model_transition *transition;
+};
+
+/** Parameters for the Settings Status message. */
+struct bt_mesh_settings_status {
+	/** The present value of the Generic OnOff state. */
+	
+    int8_t txp_present;
+
+	/** Remaining time value in milliseconds. */
+	int32_t remaining_time;
+};
